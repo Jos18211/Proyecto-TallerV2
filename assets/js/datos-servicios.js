32fc66c8aeac11ee352096506fe9256fbdb0677c
Jos18211 <100321159+Jos18211@users.noreply.github.com>
2022-08-17 02:15:17 -0600

    firts commit
    Joseph Quesada Salas
    josep18211gmail.com

---
+++ assets/js/datos-servicios.js
@@ -0,0 +1,54 @@
+window.onload = callService;
+var datos;
+var urlApp = "https://jos18211.github.io/serviciosjson/servicios.json";
+
+function callService() {
+    $.ajax({
+        url: urlApp,
+        type: "get",
+        dataType: "json",
+        success: onSuccess,
+        error: onError
+    });
+}
+
+function onSuccess(data) {
+    console.log(data);
+    datos = data;
+    procesarDatos();
+
+}
+
+function onError(jqXHR, textStatus, errorThrow) {
+    alert("mensaje de error: " + errorThrow + "\nURL " + urlApp);
+}
+
+
+function procesarDatos() {
+    let divnacionalidad = document.getElementById("pintar-servicios");
+    let htmlDinamico = "";
+
+    datos.servicios.forEach(servicio => {
+
+
+
+        htmlDinamico += "<div class='col-lg-4 col-md-6 mt-4 mt-md-0'>";
+        htmlDinamico += "  <div class='box recommended' data-aos='zoom -in '>";
+        htmlDinamico += "<h3>" + servicio.nombre + "</h3>";
+        htmlDinamico += "<h4><span>Desde</span><sup>₡</sup>" + servicio.precio + "</h4>";
+        htmlDinamico += "<ul>";
+        htmlDinamico += "<li>" + servicio.descripcion + "</li>";
+        htmlDinamico += "   </ul>";
+        htmlDinamico += " </div>";
+        htmlDinamico += "</div>";
+
+
+
+    });
+
+    divnacionalidad.innerHTML = "";
+    divnacionalidad.innerHTML = htmlDinamico;
+    console.log(htmlDinamico);
+}
+
+
---
+++ enviar.php
@@ -0,0 +1,27 @@
+<!--En un archivo separado que guardarás con extensión .php y el cual se debe llamar enviar.php, pega el siguiente código:
+      
+<?php
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'josep18211@gmail.com';
+$asunto = 'Asunto'. $subject." \r\n";
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
+-->
